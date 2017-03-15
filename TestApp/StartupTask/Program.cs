e0dde1f63fafc11eab78da52bc67d1b008b9270d
Sean Feldman <feldman.sean@gmail.com>
2017-03-15 00:35:39 -0600

    Initial commit

---
+++ TestApp/StartupTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Fabric;
+using System.Fabric.Query;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartupTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            await Console.Out.WriteLineAsync("Starting StartupTask");
+
+            var serviceTypeName = args.FirstOrDefault();
+            if (string.IsNullOrEmpty(serviceTypeName))
+                throw new ArgumentNullException(nameof(serviceTypeName));
+
+            using (var client = new FabricClient())
+            {
+                var activationContext = FabricRuntime.GetActivationContext();
+                var nodeContext = FabricRuntime.GetNodeContext();
+                var nodeName = nodeContext.NodeName;
+                var applicationName = new Uri(activationContext.ApplicationName);
+
+                var deployedServiceReplicaList = await client.QueryManager.GetDeployedReplicaListAsync(nodeName, applicationName);
+                var instance = deployedServiceReplicaList.OfType<DeployedStatelessServiceInstance>()
+                    .FirstOrDefault(svcInstance => svcInstance.ServiceTypeName == serviceTypeName);
+
+                if (instance == null)
+                {
+                    throw new InvalidOperationException($"Unable to find a service instance for {serviceTypeName}");
+                }
+                
+                File.WriteAllText("FabricData.txt", instance.InstanceId.ToString());
+
+                await Console.Out.WriteLineAsync("Finishing StartupTask");
+            }
+        }
+    }
+}
---
+++ TestApp/StatelessOne/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace StatelessOne
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+//                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "FabricData.txt");
+                var fullPath = Path.Combine(AssemblyDirectory, "FabricData.txt");
+                var allText = File.ReadAllText(fullPath);
+                ServiceEventSource.Current.Message("Instance ID read from file: {0}", allText);
+
+                // The ServiceManifest.XML file defines one or more service type names.
+                // Registering a service maps a service type name to a .NET type.
+                // When Service Fabric creates an instance of this service type,
+                // an instance of the class is created in this host process.
+
+                ServiceRuntime.RegisterServiceAsync("StatelessOneType",
+                    context => new StatelessOne(context)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(StatelessOne).Name);
+
+                // Prevents this host process from terminating so services keep running.
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
